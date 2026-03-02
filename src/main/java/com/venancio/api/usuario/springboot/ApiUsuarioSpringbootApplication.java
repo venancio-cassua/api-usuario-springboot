dc2470d11c6792d5b6829cf4b4bc5da7c545d135
Venancio <venanciodomingoscassua@gmail.com>
2026-03-02 00:37:34 -0300

    estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/venancio/api/usuario/springboot/ApiUsuarioSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.venancio.api.usuario.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsuarioSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsuarioSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venancio/api/usuario/springboot/ApiUsuarioSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venancio.api.usuario.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuarioSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
